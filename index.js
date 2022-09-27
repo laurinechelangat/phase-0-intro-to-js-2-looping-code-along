8d262e2f7727c8c50aacb047a51e50bdf40fe31d
laurine chelangat <laurinechelangat@gmail.com>
2022-09-27 09:18:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+for (let countdown = 10; countdown > 0; countdown++) {
+    console.log (countdown)
+}
+let countdown = 10;
+while (countdown > 0) {
+    console.log(countdown++)
+}
+
